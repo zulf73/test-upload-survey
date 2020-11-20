d030a2460c325e955a039563a88a128f5f245ef5
Zulf Ahmed <zulf@pop-os.localdomain>
2020-11-20 09:27:32 -0600

    Simple file upload.  Bare bones.  no checking.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    
+
+    render() {
+	return (
+		<div className="App">
+		<form action="/uploadfile" enctype="multipart/form-data" method="POST"> 
+		<input type="file" name="myFile" />
+		<input type="submit" value="Upload a file"/>
+		</form>
+		</div>
+	);
+    }
 }
+
 
 export default App;
